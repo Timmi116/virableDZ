dc5b955c4fb78fa5d122836929628ec7cd6b9d50
Timofei <timmi202116202@gmail.com>
2024-06-21 16:25:34 +0300

    Add files via upload

---
+++ variableDZ.py
@@ -0,0 +1,9 @@
+homework = 12
+watch = 1.5
+well = ('Python')
+Time_per_task = watch / homework
+print('Курс:', well, ',всего задач:', homework, ',затрачено часов:', watch,
+      ',среднее время выполнения:', Time_per_task,'часа')
+
+
+
